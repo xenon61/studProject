21c76a35b4b23a6e014b46411e2669f2e829da5e
xenon <xenon61@gmail.com>
2021-08-09 15:58:45 +0600

    first program started

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-alert("check!");
+alert("Welcome to KinoPoisk online!");
+
+let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
+
+const personalMovieDB = {
+        count: numberOfFilms,
+        movies: {},
+        actors: {},
+        genres: [],
+        private: false
+};
+
+let lastSeenMovie = prompt("Один из последних просмотренных фильмов?"),
+     myRaiting = prompt("Насколько вы оцениваете его??");
+
+// const movies = {
+//     name: lastSeenMovie,
+//     rate: myRaiting
+// };
+personalMovieDB.movies[lastSeenMovie] = myRaiting;
+
+console.log(personalMovieDB);
